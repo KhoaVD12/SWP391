7b9f53021eea2cd3eeb2ba0f72a2429d7b180e66
Đinh Văn Phong <phongdvse171245@fpt.edu.vn>
2024-08-19 22:06:31 +0700

    Add some validation for attendee

--- Ticket/BusinessObject/Models/AttendeeDTO/RegisterAttendeeDTO.cs
+++ Ticket/BusinessObject/Models/AttendeeDTO/RegisterAttendeeDTO.cs
@@ -1,16 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BusinessObject.Models.AttendeeDto;
 
 public class RegisterAttendeeDTO
 {
     public DateTime RegistrationDate { get; set; }
+    [Required]
     public int TicketId { get; set; }
+    [Required]
     public int EventId { get; set; }
     public List<AttendeeDetailDto> AttendeeDetails { get; set; }
 }
 
 public class AttendeeDetailDto
 {
+    [Required]
+    [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")]
     public string Name { get; set; }
+    [Required]
+    [EmailAddress(ErrorMessage = "Invalid email address.")]
     public string Email { get; set; }
+    [Required]
+    [Phone(ErrorMessage = "Invalid phone number.")]
     public string Phone { get; set; }
 }
