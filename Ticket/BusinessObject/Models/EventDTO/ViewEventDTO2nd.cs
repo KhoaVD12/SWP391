700f34b824798421d0dc44877a75fdcfee7e60de
KhoaVD <khoavdse171494@fpt.edu.vn>
2024-08-23 20:49:26 +0700

    Add GetEventByOrganizer() and ViewEventDTO2nd

---
+++ Ticket/BusinessObject/Models/EventDTO/ViewEventDTO2nd.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.Models.EventDTO
+{
+    public class ViewEventDTO2nd
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public int StaffId { get; set; }
+        public string StaffName { get; set; }
+        public int VenueId { get; set; }
+        public string VenueName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string? Image { get; set; }
+        public string Status { get; set; }
+        public EventTicket Ticket { get; set; }
+    }
+    public class EventTicket
+    {
+        public int Id { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int EventId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime TicketSaleEndDate { get; set; }
+    }
+}
--- Ticket/DataAccessObject/Repo/EventRepo.cs
+++ Ticket/DataAccessObject/Repo/EventRepo.cs
@@ -83,7 +83,12 @@ namespace DataAccessObject.Repo
         }
         public async Task<IEnumerable<Event>>GetEventByOrganizer(int organizerId)
         {
-            return await _context.Events.Include(e=>e.Organizer).Include(e=>e.Venue).Where(e => e.OrganizerId ==organizerId).ToListAsync();
+            return await _context.Events
+                .Include(e=>e.Organizer)
+                .Include(e=>e.Staff)
+                .Include(e=>e.Tickets)
+                .Include(e=>e.Venue)
+                .Where(e => e.OrganizerId ==organizerId).ToListAsync();
         }
         public async Task<IEnumerable<Attendee>> GetAttendeesByEventAsync(int eventId)
         {
