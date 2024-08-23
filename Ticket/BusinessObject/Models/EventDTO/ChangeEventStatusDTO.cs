dfca720a1e93ec07ab8df1e10b148ede0a32f1c2
Đinh Văn Phong <phongdvse171245@fpt.edu.vn>
2024-08-23 21:58:03 +0700

    Add some validation

--- Ticket/BusinessObject/Models/EventDTO/ChangeEventStatusDTO.cs
+++ Ticket/BusinessObject/Models/EventDTO/ChangeEventStatusDTO.cs
@@ -1,6 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BusinessObject.Models.EventDTO;
 
 public class ChangeEventStatusDTO
 {
+    [Required(ErrorMessage = "Status is required.")]
+    [RegularExpression("^(Pending|Active|Ended|Cancelled)$",
+        ErrorMessage = "Status must be one of the following values: 'Pending', 'Active', 'Ended', 'Cancelled'.")]
     public string Status { get; set; }
 }
