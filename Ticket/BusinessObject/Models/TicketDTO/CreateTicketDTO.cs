21a3adcfd98053bed94c4ed256eca57f31e544ae
Đinh Văn Phong <phongdvse171245@fpt.edu.vn>
2024-08-28 18:11:38 +0700

    Remove validation

--- Ticket/BusinessObject/Models/TicketDTO/CreateTicketDTO.cs
+++ Ticket/BusinessObject/Models/TicketDTO/CreateTicketDTO.cs
@@ -9,17 +9,11 @@ namespace BusinessObject.Models.TicketDTO
 {
     public class CreateTicketDTO
     {
-        [Range(10000, 10000000, ErrorMessage = "Price must be at least 10,000 and not exceed 10000000")]
         public decimal Price { get; set; }
 
-        [Required(ErrorMessage = "EventId is required.")]
         public int EventId { get; set; }
 
-        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1.")]
         public int Quantity { get; set; }
-
-        [Required(ErrorMessage = "Ticket sale end date is required.")]
-        [DataType(DataType.DateTime)]
         public DateTime TicketSaleEndDate { get; set; }
     }
 }
